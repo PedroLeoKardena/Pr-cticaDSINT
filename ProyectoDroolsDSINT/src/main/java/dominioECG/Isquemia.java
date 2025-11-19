6dca46609424462fbefa2e50c768e3fe9d2e8131
Eduardo Sánchez Heredia <eduardosanchezheredia13@gmail.com>
2025-11-19 08:46:36 +0100

    Isquemia Coronaria

--- ProyectoDroolsDSINT/src/main/java/dominioECG/Isquemia.java
+++ ProyectoDroolsDSINT/src/main/java/dominioECG/Isquemia.java
@@ -1,9 +1,31 @@
 package dominioECG;
 
 public class Isquemia extends Diagnostico{
-
+	
+	private int nCicloAfectado;
+	private double amplitudQRS;
+	private double amplitudT;
+	
 	public Isquemia(String descripcion, String nombreDiagnostico) {
 		super(descripcion, nombreDiagnostico);
 	}
-
+	
+	public int getnCicloAfectado() {
+		return nCicloAfectado;
+	}
+	public void setnCicloAfectado(int nCicloAfectado) {
+		this.nCicloAfectado = nCicloAfectado;
+	}
+	public double getAmplitudQRS() {
+		return amplitudQRS;
+	}
+	public void setAmplitudQRS(double amplitudQRS) {
+		this.amplitudQRS = amplitudQRS;
+	}
+	public double getAmplitudT() {
+		return amplitudT;
+	}
+	public void setAmplitudT(double amplitudT) {
+		this.amplitudT = amplitudT;
+	}
 }
